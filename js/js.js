0d61867a1bcaec06c11843affb993e5754b0a778
djama <8djama8@mail.ru>
2023-04-27 11:16:23 +0300

    виаи

---
+++ js/js.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search');
+const fries = document.querySelector('.fries');
+
+function toggleMenu(){
+    search.classList.toggle('open');
+    fries.classList.toggle('open');
+}
